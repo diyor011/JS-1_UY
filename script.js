77dbb37495ac9c9fee1b720e80fd15827f1e4edd
dior011 <diyorraxmatullaev3@gmail.com>
2022-04-30 22:21:02 +0500

    master011

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+var str = prompt('Ismingizni kiriting' )
+
+var num = prompt('Yowizi kiritn')
+alert('Sizning ismingiz ' + str + ' yowiz ' + num)
+
+
+var str2 = prompt('misol yecamizmi')
+alert('sizi jovobiz ' +  str)
+
+ 
+var num1 = prompt('8 + 2 =')
+var num2 = prompt('8 - 2 =')
+var num3 = prompt('8 * 2 =')
+var num4 = prompt('8 / 2 =')
+
+
+console.info( 'Sizning ismingiz ' + str)
+console.info('Sizning yowingz ' + num)
+
+console.info('sizning jovob 8 + 2 = ' + num1 + (' togri jovob ' + '10 '))
+console.info('sizning jovob 8 - 2 = ' + num2 + (' togri jovob ' + '6 '))
+console.info('sizning jovob 8 * 2 = ' + num3 + (' togri jovob ' + '16 '))
+console.info('sizning jovob 8 / 2 = ' + num4 + (' togri jovob ' + '4 '))
